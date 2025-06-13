7e845026e45d8cd6028c85dc4fddb41171ee3b2f
gabriel <gabrielduartesilvan@gmail.com>
2025-06-13 16:31:42 -0300

    commit inicial

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(params) {
+    return (
+        <header className="header" style={{background: 'red'}}>
+            <div className="container">
+                aaa
+            </div>
+        </header>
+    );
+}
+
+export default Header;
