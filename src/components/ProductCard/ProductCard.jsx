35a350fd52f7ccd22ebdba37c4d6690932948593
aryad <aryad@ARYADNE>
2025-06-15 16:05:37 -0300

    criacao do card

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BsCartPlus } from "react-icons/bs";
+
+import './ProductCard.css';
+
+function ProductCard() {
+    return (
+        <section className="product_card">
+
+            <img 
+                src="https://m.media-amazon.com/images/I/51fYXSnSu9L._AC_UY654_FMwebp_QL65_.jpg" alt="product"
+                className="card_image" />
+
+            <div className="card_infos">
+                <h2 className="card_price">R$ 200.20</h2>
+                <h2 className="card_title">titulo do produto</h2>
+            </div>
+
+            <button
+                type="button" className="button_add-cart">
+                <BsCartPlus />
+            </button>
+
+        </section>
+    );
+}
+
+export default ProductCard;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import './Products.css';
 import fetchProducts from "../../api/fetchProducts";
+import ProductCard from "../ProductCard/ProductCard";
 
 function Products() {
     const [products, setProducts] = useState([])
@@ -15,7 +16,7 @@ function Products() {
     }, [])
     return (
         <section className="products container">
-            products
+            <ProductCard />
         </section>
     );
 }
